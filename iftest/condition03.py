d12aedcee0a56e7edfe82948b4e28a34f34e3432
purohita57 <purohita57@users.noreply.github.com>
2021-09-22 14:04:07 +0000

    if test

---
+++ iftest/condition03.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+hostname = input("What value should we set for hostname?")
+if hostname.lower() == "mtg":
+   print("hostname matches expected config")
+## Always print out to the user
+   print("Exiting the script")
